db7da75a7b561fce8e1ae9a630302ec8fb6e417f
Koen Verburg <creativekoen@gmail.com>
2020-05-10 22:27:05 +0200

    refactor(index - page): added basic structure

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 // tslint:disable-next-line:no-submodule-imports
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/style.styl'
-
+import * as F from '@features'
 export const config = { amp: 'hybrid' }
 
 const Home = () => (
-  <main>
-    <article>
-      <h1>Hashtag de-Duplication </h1>
-      <p>Simple to use web app that dedups Hashtags</p>
-    </article>
-  </main>
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-xs-12 col-md-8">
+        <F.Collector />
+      </div>
+    </div>
+  </div>
 )
 
 export default Home
