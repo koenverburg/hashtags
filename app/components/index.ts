98fcada2dbd1403178a9dd050e6cb8e22bd61b65
Koen Verburg <creativekoen@gmail.com>
2020-06-30 22:43:04 +0200

    refactor(chakra): removed the exports

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,4 @@
 /* tslint-disable */
 /* GENERATOR-IMPORT */
-export { Textarea } from './Textarea'
-export { HashTagCollector } from './HashTagCollector'
 export { Clock } from './Clock'
 export { App } from './App'
