e1d5952ae8aa626f17fb3179f8696f9769775de2
Koen Verburg <creativekoen@gmail.com>
2020-07-01 22:25:21 +0200

    refactor(hooks,mobx): this example is not needed any more

--- app/features/example/__tests__/Domain.store.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DomainStore } from '../Domain.store'
-
-describe('DomainStore', () => {
-  it('fetches domains', () => {
-    const store = new DomainStore()
-    expect(store.fetch).toBeDefined()
-  })
-})
--- app/features/example/models/Domain.model.ts
+++
@@ -1,8 +0,0 @@
-import { observable } from 'mobx'
-
-export class DomainModel {
-  @observable public id: number
-  @observable public host: string
-  @observable public www: string
-  @observable public displayName: string
-}
