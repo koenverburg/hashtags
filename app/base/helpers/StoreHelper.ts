871a98a00144bbd33ef73e83a7575942b557b57e
Koen Verburg <creativekoen@gmail.com>
2020-07-01 22:22:01 +0200

    refactor(hooks): removed some unnecessary elements to make it easer in development but still hold the ability to abstract
    the any method will be rewriten

--- app/base/helpers/StoreHelper.ts
+++ app/base/helpers/StoreHelper.ts
@@ -6,19 +6,10 @@ import { IInitialState } from '../interfaces/IInitialState'
 import { StatusHelper } from './StatusHelper'
 import { NextMobx } from '@helpers'
 
-interface IHelperLength {
-  length?: number
-}
-
 // tslint:disable-next-line: ban-types
-export abstract class StoreHelper<EntityType extends IHelperLength> extends StatusHelper implements IBaseStore {
+export abstract class StoreHelper<EntityType> extends StatusHelper implements IBaseStore {
   constructor(props?: IInitialState<EntityType>) {
     super()
-    // tslint:disable-next-line:forin
-    for (const prop in props) {
-      // @ts-ignore
-      this[prop] = NextMobx.jsonToMap(props[prop])
-    }
   }
 
   @observable public error: AxiosError
@@ -26,24 +17,17 @@ export abstract class StoreHelper<EntityType extends IHelperLength> extends Stat
 
   public abstract fetch(): void
 
-  @action public update(data: object = {}): void {
-    // tslint:disable-next-line:forin
-    for (const prop in data) {
-      this[prop] = data[prop]
-    }
-  }
+  // public any(): boolean {
+  // //   if (!this.entities) {
+  // //     return false
+  // //   }
 
-  public any(): boolean {
-    if (!this.entities) {
-      return false
-    }
+  // //   if (this.entities.length) {
+  // //     return this.entities.length > 0
+  // //   }
 
-    if (this.entities.length) {
-      return this.entities.length > 0
-    }
-
-    return false
-  }
+  //   return false
+  // }
 
   @action.bound public setData(entities: EntityType) {
     this.entities = entities
--- app/base/interfaces/IBaseStore.ts
+++ app/base/interfaces/IBaseStore.ts
@@ -4,8 +4,5 @@ export interface IBaseStore {
   IsCanceled: boolean
   IsFulfilled: boolean
   IsRejected: boolean
-  any(): boolean
-
-  // next-mobx-wrapper baseStore https://github.com/nghiepit/next-mobx-wrapper/blob/master/src/index.js
-  update(data: object): void
+  // any(): boolean
 }
